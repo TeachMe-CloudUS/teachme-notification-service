09a9ee546d6c6410fbddc6cfc6b95ba2b695b7a5
Rami Nakkar <rami.nakkar00@gmail.com>
2025-01-04 23:12:22 +0100

    Add handling for CertificateCreatedEvent and notifications
    Introduce `CertificateCreatedEvent` to represent certificate creation events. Implement `CertificateCreatedEventHandler` to process these events by saving notifications to the database and sending them via WebSocket. This provides a complete workflow for notifying users of their course completion and certificate availability.

---
+++ src/main/java/us/cloud/teachme/notification_service/application/service/handler/CertificateCreatedEventHandler.java
@@ -0,0 +1,68 @@
+package us.cloud.teachme.notification_service.application.service.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import us.cloud.teachme.notification_service.application.dto.NotificationContent;
+import us.cloud.teachme.notification_service.application.ports.WebSocketPort;
+import us.cloud.teachme.notification_service.domain.Notification;
+import us.cloud.teachme.notification_service.domain.event.CertificateCreatedEvent;
+import us.cloud.teachme.notification_service.infrastructure.persistence.MongoNotificationRepository;
+
+import java.time.Instant;
+
+@Service
+@RequiredArgsConstructor
+public class CertificateCreatedEventHandler implements KafkaEventHandler {
+
+    private static final String TOPIC = "certification-service.certificate.created";
+
+    private final MongoNotificationRepository repository;
+    private final ObjectMapper mapper;
+    private final WebSocketPort webSocketPort;
+
+    @Override
+    public boolean canHandle(String topic) {
+        return TOPIC.equals(topic);
+    }
+
+    @Override
+    public void handle(String message) {
+        try {
+            var event = mapper.readValue(message, CertificateCreatedEvent.class);
+
+            var entity = Notification.builder()
+                    .title("Welcome to TeachMe!")
+                    .userId(event.getUserId())
+                    .message("""
+                            <div class="notification-content">
+                                <h4>Congratulations, %s!</h4>
+                                <p>We're excited to share that you've successfully completed your course "%s" and your certificate is ready for download!</p>
+                                <p>This is a significant milestone, and we're proud to celebrate your achievement with you.</p>
+                                <p>Here’s what you can do next:</p>
+                                <ul>
+                                    <li>Access your <strong>Courses</strong> to view and download your certificate.</li>
+                                    <li>Share your accomplishment with your network to showcase your expertise.</li>
+                                    <li>Explore new courses in our <strong>Course Library</strong> to keep growing your skills.</li>
+                                </ul>
+                                <p>If you have any questions or need assistance, feel free to visit our <a href="/student/help">Help Center</a>.</p>
+                                <p>Once again, congratulations on your achievement! Keep up the great work!</p>
+                            </div>
+                            """.formatted(event.getStudentName() + " " + event.getStudentSurname(), event.getCourseName()))
+                    .previewText("Congratulations, %s! We're excited to share that you've successfully completed your course \"%s\" and your certificate is ready for download!"
+                            .formatted(event.getStudentName() + " " + event.getStudentSurname(), event.getCourseName()))
+                    .type("CERTIFICATE_CREATED")
+                    .timestamp(Instant.now())
+                    .build();
+
+            var content = NotificationContent.create(entity);
+
+            repository.save(entity);
+
+            webSocketPort.sendNotification(content);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/us/cloud/teachme/notification_service/domain/event/CertificateCreatedEvent.java
@@ -0,0 +1,19 @@
+package us.cloud.teachme.notification_service.domain.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class CertificateCreatedEvent extends DomainEvent {
+
+    private String userId;
+    private String eventName;
+    private String courseName;
+    private String studentName;
+    private String studentSurname;
+}
